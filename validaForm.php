c453a70b0af17097e5ba94274b31e61927808bd8
victormedeiros <victor.medeiros@contraprova.com.br>
2020-01-14 17:07:44 -0300

    Validação de Form por Lenght e Value

--- validaForm.php
+++ validaForm.php
@@ -1,35 +1,37 @@
+
 <html>
+
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>Validador de Form</title>
+    <title>Valida Form JS</title>
     <meta charset="utf-8">
 </head>
+
 <body>
-    <div class="container">         
-        <form action="" onsubmit="return validarForm()">            
-            <div class="row mt-3">
-                <div class="form-group">
-                    <label>Digite uma informação</label>                        
-                    <input type="text" class="form-control mb-3" id="nome">
-                    <button class="btn btn-primary">Enviar</button>
-                </div>    
-            </div>                
-        </form>
+    <div class="container">
+        <h1>Valida Form JS</h1>
     </div>
-<script>              
-        function validarForm(){
-            if($("#codigo").val().length < 10){
-                alert("Dado Digitado não possui 10 caracteres");
-                return false;
-            }else{
-                alert("Tudo Ok");
-                return true;
+    <form action="" id="formulario">
+        <input type="text" name="" id="nome">
+        <input type="email" name="" id="email">
+        <button type="submit" id="botao">Clicar</button>
+    </form>
+
+    <script>
+        btn = document.querySelector('#botao');
+        nome = document.querySelector('#nome');
+        email = document.querySelector('#email');
+
+
+        form = document.querySelector('#formulario').addEventListener('submit', verificaForm);
+
+        function verificaForm(e) {
+            if (nome.innerText.length < 3 || !email.value) {
+                alert('Preencha os Campos');
+                e.preventDefault();
             }
-        }            
-</script>
+        }
+    </script>
+
 </body>
-    
+
 </html>
